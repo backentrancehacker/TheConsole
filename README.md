dd1ed5d4486458e5d590dbad56a3d137f472c35a
NathanFS <backentrancehacker@gmail.com>
2020-07-05 11:22:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# TheConsole
+A completely useless garbage console that you can drag around
+
+## Features
+* Simple design
+* Close button
+* Drag and Drop
+
+## Methods
+`console.log`: logs a white message to the console
+`console.success`: logs a green message to the console
+`console.info`: logs a blue message to the console
+`console.warn` and `console.error`: logs a red message to the console
+All errors are automatically caught and passed to `console.warn`.
+
+## "Usage"
+Since all console methods are overrided, you can use `console.log` just as normal.
---
+++ script.js
@@ -0,0 +1,36 @@
+const CONSOLE = document.getElementById('console')
+const CLOSE = document.getElementById('close')
+const TOP = document.getElementById('top')
+const CONTENT = document.getElementById('content').querySelector('pre')
+
+const formatConsole = (elem) => {
+	elem.innerHTML = 'TheConsole AdCharity 2020'
+	const colors = {
+		warn: '#FF0000',
+		error: '#FF0000',
+		success: '#7FFF00',
+		info: '#1E90FF',
+		log: '#FFFFFF'
+	}
+	let temp = {}
+	for(let key in colors) {
+		temp[key] = (words) => {
+			if(typeof words === 'object') 
+				words = JSON.stringify(words, null, 2)
+			elem.innerHTML += `<span style="color: ${colors[key]}">${words}</span>`
+		}
+	}
+	temp.clear = () => elem.innerHTML = 'TheConsole AdCharity 2020'
+	return temp
+}
+
+CLOSE.addEventListener('click', () => {
+	CONSOLE.remove()
+})
+
+window.console = formatConsole(CONTENT)
+window.onerror = msg => console.warn(msg)
+
+console.success('Loaded Console!')
+
+drags(CONSOLE)
